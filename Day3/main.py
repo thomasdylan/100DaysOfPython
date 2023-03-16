7f85eaecf191b9b00a85f527d8b8bb4f2f896e95
Dylan Thomas <thomasdylan76@gmail.com>
2023-03-15 23:12:23 -0600

    Create main.py

---
+++ Day3/main.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+# https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+# Write your code below this line 👇
+question1 = input(
+    "You have two paths ahead of you, which do you choose? left or right ")
+
+if question1.lower() == "left":
+    question2 = input(
+        "You walk down the left path till it ends at a river. The waters are fast here. Do you swim or walk upstream until you find a safe place to pass? swim or safe ")
+    if question2.lower() == "safe":
+        question3 = input(
+            "You safely cross the river and find three doors ahead of you. A red door, a blue door and a yellow door. Which do you choose? red or blue or yellow ")
+        if question3.lower() == "blue":
+            print("Congradulations! You win!")
+        else:
+            print(
+                "You chose wrong and have been banished to the shadow realm.\nGame Over")
+    else:
+        print("The river carries you away. Game Over")
+else:
+    print("You keep walking for the rest of time. Stuck on a path with no end. Game Over")
