4cfe3813144e460d73a9022caaff067e4a49cc33
Dylan Thomas <thomasdylan76@gmail.com>
2023-03-18 14:29:13 -0600

    Day 7: Hangman game

---
+++ Day7/main.py
@@ -0,0 +1,50 @@
+import random
+import hangman_word_list
+import hangman_stages
+
+# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+chosen_word = random.choice(hangman_word_list.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+print(hangman_stages.logo)
+# Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+# Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You have already guessed {guess}")
+    # Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(
+            f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    # Check if user is wrong.
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    # Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_stages.stages[lives])
