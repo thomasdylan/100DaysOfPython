b7766d773bc3cf0dc6a51d729da42a11af80a1bd
Dylan Thomas <thomasdylan76@gmail.com>
2023-03-23 13:06:37 -0600

    Day 13. Debugging
    All code is commented out.  Each block can be run to see the error and the fix is listed in a comment below.

---
+++ Day13/main.py
@@ -0,0 +1,69 @@
+############ DEBUGGING#####################
+
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 20):
+
+# Line above needs to be changed - for i in range (1, 21):
+
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+
+# The line above needs to be changed - dice_num = randint(0, 5)
+
+# print(dice_imgs[dice_num])
+
+
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year > 1994:
+
+# The line above needs to be changed - elif year >= 1994
+
+#   print("You are a Gen Z.")
+
+
+# # Fix the Errors
+# age = input("How old are you?")
+
+# The line above needs to be changed - age = int(input("How old are you?"))
+
+# if age > 18:
+# print("You can drive at age {age}.")
+
+# The line above needs to be changed - print(f"You can drive at age {age}.")
+
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+
+# The line above needs to be changed - word_per_page = int(input("Number of words per page: "))
+
+# total_words = pages * word_per_page
+# print(total_words)
+
+
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)
+
+# The line above needs to be indented in the for loop
+
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
