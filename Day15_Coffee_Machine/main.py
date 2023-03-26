19094018833d64507c039986439ae4edc94f6fc9
Dylan Thomas <thomasdylan76@gmail.com>
2023-03-26 10:35:31 -0600

    Day 15: Coffee Machine

--- Day15_Coffee_Machine/main.py
+++ Day15_Coffee_Machine/main.py
@@ -32,11 +32,41 @@ resources = {
 
 
 def is_resource_efficient(order_ingredients):
+    """Returns True when order can be made and false when ingredients are insufficient"""
     for item in order_ingredients:
         if order_ingredients[item] >= resources[item]:
             print(f"Sorry there is not enough {item} to create this drink.")
             return False
     return True
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.10
+    total += int(input("how many nickels?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True whe the payment is accepted and False when not."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is your {change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that is not enough. Money has been refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}. Enjoy!")
 
 
 profit = 0
@@ -52,4 +82,7 @@ while is_on:
         print(f"Water: {resources['water']}ml  Milk: {resources['milk']}ml  Coffee: {resources['coffee']}g")
     else:
         drink = MENU[choice]
-        is_resource_efficient(drink["ingredients"])
+        if is_resource_efficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
