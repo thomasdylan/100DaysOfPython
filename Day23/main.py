81a0ebf3336f450da0da6cd9a9cb4ffe764ba937
Dylan Thomas <thomasdylan76@gmail.com>
2023-05-10 18:44:46 -0600

    Day 23

--- Day23/main.py
+++ Day23/main.py
@@ -8,7 +8,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
