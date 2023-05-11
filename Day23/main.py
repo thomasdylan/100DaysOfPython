12c429e881cd3ade0938005f3e815df03eabad65
Dylan Thomas <thomasdylan76@gmail.com>
2023-05-10 19:24:46 -0600

    Added collision to frogger rip off game

--- Day23/main.py
+++ Day23/main.py
@@ -21,3 +21,15 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    # Detect collision
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    # Detect Finish
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+
+screen.exitonclick()
